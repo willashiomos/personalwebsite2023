1207db7fa713253986ec929839f537364aaee8db
willashiomos <willashiomos@gmail.com>
2023-07-25 15:54:25 -0400

    rotate desktop/mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,46 @@
 import "../styles/index.scss";
 
-const SHAPES_TO_ROTATE = [1, 2, 3, 4, 5, 7, 9, 11];
-const ROTATE_INTERVAL = 100000;
+const SHAPES_TO_ROTATE_DESKTOP = [[1, 2, 3, 4, 5, 7, 9, 11]];
+const SHAPES_TO_ROTATE_MOBILE = [
+  [1, 2, 3, 4, 5],
+  [1, 3, 5, 6],
+];
+const ROTATE_INTERVAL = 10000;
 const TIMEOUT_INTERVAL = 4000;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const images = document.querySelector(".images");
+  var images;
+  var shapesToRotate;
+  desktopOrMobile();
   window.setInterval(rotateRandomImg, ROTATE_INTERVAL);
 
+  window.addEventListener("resize", () => {
+    desktopOrMobile();
+  });
+
+  function desktopOrMobile() {
+    if (window.innerWidth < 1000) {
+      images = document.querySelectorAll(".images--mobile");
+      shapesToRotate = JSON.parse(JSON.stringify(SHAPES_TO_ROTATE_MOBILE));
+    } else {
+      images = document.querySelectorAll(".images--desktop");
+      shapesToRotate = JSON.parse(JSON.stringify(SHAPES_TO_ROTATE_DESKTOP));
+    }
+  }
+
   function rotateRandomImg() {
-    var randomImg =
-      SHAPES_TO_ROTATE[Math.floor(Math.random() * SHAPES_TO_ROTATE.length)];
-    var selectedImg = images.querySelector("img:nth-child(" + randomImg + ")");
-    selectedImg.classList.add("rotateAnim");
-    setTimeout(() => {
-      selectedImg.classList.remove("rotateAnim");
-    }, TIMEOUT_INTERVAL);
+    images.forEach((imageGroup, idx) => {
+      var randomImg =
+        shapesToRotate[idx][
+          Math.floor(Math.random() * shapesToRotate[idx].length)
+        ];
+      var selectedImg = imageGroup.querySelector(
+        "img:nth-child(" + randomImg + ")"
+      );
+      selectedImg.classList.add("rotateAnim");
+      setTimeout(() => {
+        selectedImg.classList.remove("rotateAnim");
+      }, TIMEOUT_INTERVAL);
+    });
   }
 });
